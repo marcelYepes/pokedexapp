bcf1710c63c13242c2b63f59cf93618e7911c6e0
marcelYepes <marcelyepesqf@gmail.com>
2023-05-08 16:16:49 -0500

    fix actualization bug

--- src/pages/ProtectedRoutes.jsx
+++ src/pages/ProtectedRoutes.jsx
@@ -1,16 +1,13 @@
-import React, {useEffect} from "react"
 import {useSelector} from "react-redux"
-import {Outlet, useNavigate} from "react-router-dom"
+import {Outlet, Navigate} from "react-router-dom"
 
 const ProtectedRoutes = () => {
   const {trainerName} = useSelector(state => state)
 
-  const navigate = useNavigate()
-
   if (trainerName.length >= 3) {
     return <Outlet />
   } else {
-    navigate("/")
+    return <Navigate to="/" />
   }
 }
 
